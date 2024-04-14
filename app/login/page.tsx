a9b31cc3ff4445718b54f38debc43e0d5c8ba5cc
Brodypen <brodypen7987@gmail.com>
2024-04-13 21:10:46 -0500

    Basic signin OAUTH

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -27,20 +27,47 @@ export default function AuthComponent() {
   return (
     <main className="min-h-screen w-screen overflow-hidden flex items-center justify-center text-center bg-gradient-radial from-#19181C to-#161518">
       <div className="fixed top-0 left-0 p-4 flex justify-between w-full px-8">
-        <Link href="/" className="text-white">
+        <Link href="/" className="text-white z-50">
           Home
         </Link>
-        <div className="flex">
+        {/* <div className="flex">
           <Link href="/about" className="text-white mr-4">
             About
           </Link>
           <Link href="/login" className="text-white">
             Login
           </Link>
-        </div>
+        </div> */}
       </div>
       <Boxes className="" />
       <div className="z-10">
+        <div className=" w-96 rounded-md border p-5 space-y-5 relative bg-slate-900">
+          <div className="flex items-center gap-2">
+            <KeyRound />
+            <h1 className="text-2xl font-bold">GitStudy Login</h1>
+          </div>
+
+          <p className="text-sm text-gray-300">Register/SignIn Today!</p>
+          <div className="flex flex-col gap-5 text-gray-900">
+            <Button
+              className=" w-full flex items-center gap-2 "
+              variant="outline"
+              onClick={() => handleLoginWithOAuth("github")}
+            >
+              <FaGithub /> Github
+            </Button>
+            <Button
+              className=" w-full flex items-center gap-2 "
+              variant="outline"
+              onClick={() => handleLoginWithOAuth("google")}
+            >
+              <FcGoogle /> Google
+            </Button>
+          </div>
+          <div className="glowBox -z-10"></div>
+        </div>
+      </div>
+      {/* <div className="z-10">
         <p className="text-6xl jersey-25">GitStudy</p>
         <p className="mt-2 tracking-wider">The future of learning</p>
         <button>
@@ -66,34 +93,7 @@ export default function AuthComponent() {
         >
           <Image src="/figma.svg" alt="Figma logo" width={24} height={24} />
         </a>
-      </div>
+      </div> */}
     </main>
-    // <div className="flex items-center justify-center w-full h-[70vh]">
-    //   <div className=" w-96 rounded-md border p-5 space-y-5 relative bg-slate-900">
-    //     <div className="flex items-center gap-2">
-    //       <KeyRound />
-    //       <h1 className="text-2xl font-bold">Next + Supabase</h1>
-    //     </div>
-
-    //     <p className="text-sm text-gray-300">Register/SignIn Today 👇</p>
-    //     <div className="flex flex-col gap-5">
-    //       <Button
-    //         className=" w-full flex items-center gap-2 "
-    //         variant="outline"
-    //         onClick={() => handleLoginWithOAuth("github")}
-    //       >
-    //         <FaGithub /> Github
-    //       </Button>
-    //       <Button
-    //         className=" w-full flex items-center gap-2 "
-    //         variant="outline"
-    //         onClick={() => handleLoginWithOAuth("google")}
-    //       >
-    //         <FcGoogle /> Google
-    //       </Button>
-    //     </div>
-    //     <div className="glowBox -z-10"></div>
-    //   </div>
-    // </div>
   );
 }
